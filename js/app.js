1105b0755b0b576bba24447642c4dde7f4a3998f
kietnt12603 <97238108+kietnt12603@users.noreply.github.com>
2022-06-15 18:55:02 +0700

    upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var play = document.getElementById("play");
+var img_video = document.getElementById("img_video");
+var modal_container = document.getElementById("modal_container");
+var close = document.getElementById('close')
+play.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
